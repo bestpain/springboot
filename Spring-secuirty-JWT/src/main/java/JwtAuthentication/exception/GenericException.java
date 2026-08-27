4bb479cdebd21901b9d86b967a6777cd17db2b11
Nitish Ranjan <36075994+bestpain@users.noreply.github.com>
2026-08-27 14:08:14 +0530

    Delete Spring-secuirty-JWT/src/main/java/JwtAuthentication/exception/GenericException.java

--- Spring-secuirty-JWT/src/main/java/JwtAuthentication/exception/GenericException.java
+++
@@ -1,9 +0,0 @@
-package JwtAuthentication.exception;
-
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-public class GenericException extends RuntimeException{
-    public GenericException(Exception exception){
-        super(exception.getMessage());
-    }
-}
